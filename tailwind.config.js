8d975673510d6cb19772c55160ed016798558172
Radhames Capellan <radhamesc@gmail.com>
2025-06-28 13:30:29 +0200

    init: proyecto inicial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,js,jsx,ts,tsx,md,mdx,html}',
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
